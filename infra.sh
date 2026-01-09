84e6bb83719c9bab2c177c36f79407c2bd13ac35
webberchagas <webberschagas@gmail.com>
2026-01-09 20:50:28 -0300

    finalização script bash

--- infra.sh
+++ infra.sh
@@ -4,15 +4,27 @@ set -euo pipefail
 #======================
 # Variáveis
 #======================
-LOCATION="brazilsouth"
+: "${LOCATION:?LOCATION não definida}"
+: "${RESOURCE_GROUP:?RESOURCE_GROUP não definido}"
+: "${SERVICEBUS_NAMESPACE:?SERVICEBUS_NAMESPACE não definido}"
+
 RANDOM_ID=$RANDOM
-
-RESOURCE_GROUP="rg-techchallenge-fiap-postech"
-STORAGE_ACCOUNT="sapostech$RANDOM_ID"
-
 CREATED=$(date +%Y-%m-%d)
 OWNER="Webber Chagas"
 TAGS="created=$CREATED owner=$OWNER"
+
+STORAGE_ACCOUNT="sapostech$RANDOM_ID"
+
+# ========================================
+# Persistir variáveis para outros scripts
+# ========================================
+ENV_FILE=".env"
+echo "RANDOM_ID=$RANDOM_ID" > $ENV_FILE
+
+FUNC_GATEWAY="func-api-gateway-$RANDOM_ID"
+FUNC_COURSE="func-course-rating-$RANDOM_ID"
+FUNC_NOTIFICATION="func-ms-notification-$RANDOM_ID"
+FUNC_REPORT="func-weekly-report-$RANDOM_ID"
 
 #=======================================================
 # Workspace log (Obrigatório para application insights)
@@ -114,7 +126,7 @@ az servicebus namespace create \
   --location $LOCATION \
   --sku Basic
 
-  echo "Function app: $FUNC_REPORT, criado com sucesso."
+  echo "Service Bus: sb-post-tech-fiap, criado com sucesso."
   echo ""
 
 #=================================
@@ -125,12 +137,16 @@ az servicebus queue create \
   --namespace-name sb-post-tech-fiap \
   --name q-ms-critical-ratings \
   --max-size 1024
+  echo "Fila: q-ms-critical-ratings, criado com sucesso."
+  echo ""
 
 az servicebus queue create \
   --resource-group $RESOURCE_GROUP \
   --namespace-name sb-post-tech-fiap \
   --name q-ms-weekly-report \
   --max-size 1024
+  echo "Fila: q-ms-weekly-report, criado com sucesso."
+  echo ""
 
 #===================================================
 # Criação das politicas de Consumer e producer no Service Bus
@@ -145,7 +161,6 @@ az servicebus namespace authorization-rule create \
   --namespace-name sb-post-tech-fiap \
   --name sb-consumer-policy \
   --rights Listen
-
 
 #==========================================================================
 # Obtem o connection string para acesso ao recurso Producer no Service Bus
@@ -162,3 +177,12 @@ az servicebus namespace authorization-rule keys list \
   --resource-group $RESOURCE_GROUP \
   --namespace-name sb-post-tech-fiap \
   --name sb-consumer-policy
+  echo "Connection strings criado com sucesso."
+  echo ""
+
+cat <<EOF >> $ENV_FILE
+FUNC_GATEWAY=$FUNC_GATEWAY
+FUNC_COURSE=$FUNC_COURSE
+FUNC_NOTIFICATION=$FUNC_NOTIFICATION
+FUNC_REPORT=$FUNC_REPORT
+EOF
--- rede.sh
+++ rede.sh
@@ -1,192 +1,186 @@
 #==============================
 # Variáveis
 #=================================
-RESOURCE_GROUP="rg-techchallenge-fiap-postech"
+source .env
+
+: "${RESOURCE_GROUP:?}"
+: "${VNET_NAME:?}"
+: "${SUBNET_FUNCTIONS:?}"
+: "${SUBNET_PRIVATE_ENDPOINT:?}"
+: "${LOCATION:?}"
 
 #=================================
-# Criar a VNet e Subnets OK
+# Criar a VNet e Subnets
 #=================================
 az network vnet create \
   --resource-group $RESOURCE_GROUP \
-  --name vnet-functions-dev \
+  --name $VNET_NAME \
   --address-prefix 10.0.0.0/16 \
-  --location brazilsouth
+  --location $LOCATION
 
--Subnet para integração das Functions - OK
+#=======================================
+# Subnet para integração das Functions
+#========================================
 az network vnet subnet create \
   --resource-group $RESOURCE_GROUP \
-  --vnet-name vnet-functions-dev \
-  --name subnet-functions \
+  --vnet-name $VNET_NAME \
+  --name $SUBNET_FUNCTIONS \
   --address-prefix 10.0.1.0/24 \
   --delegations Microsoft.Web/serverFarms
 
-Subnet para Private Endpoints OK
+#=================================
+# Subnet para Private Endpoints
+#=================================
 az network vnet subnet create \
   --resource-group $RESOURCE_GROUP \
-  --vnet-name vnet-functions-dev \
-  --name subnet-private-endpoints \
+  --vnet-name $VNET_NAME \
+  --name $SUBNET_PRIVATE_ENDPOINT \
   --address-prefix 10.0.2.0/24 \
   --disable-private-endpoint-network-policies true
 
-
-2. Criar as Private DNS Zones OK
+#=================================
+# Criar as Private DNS Zones
+#=================================
 az network private-dns zone create \
   --resource-group $RESOURCE_GROUP \
   --name privatelink.servicebus.windows.net
 
-Azure Functions OK
+# Azure Functions OK
 az network private-dns zone create \
   --resource-group $RESOURCE_GROUP \
   --name privatelink.azurewebsites.net
 
-Linkar DNS à VNet OK
+#=================================
+# Linkar DNS à VNet OK
+#=================================
 az network private-dns link vnet create \
   --resource-group $RESOURCE_GROUP \
   --zone-name privatelink.servicebus.windows.net \
   --name link-sb \
-  --virtual-network vnet-functions-dev \
+  --virtual-network $VNET_NAME \
   --registration-enabled false
   
 az network private-dns link vnet create \
   --resource-group $RESOURCE_GROUP \
   --zone-name privatelink.azurewebsites.net \
   --name link-functions \
-  --virtual-network vnet-functions-dev \
+  --virtual-network $VNET_NAME \
   --registration-enabled false
-  
-  
-3. Criar Private Endpoint do Service Bus SOMENTE PARA PREMIUM
-az network private-endpoint create \
-  --name pe-servicebus \
+
+#=======================================
+# Integrar Functions privadas à VNet
+#=======================================
+az functionapp vnet-integration add \
   --resource-group $RESOURCE_GROUP \
-  --vnet-name vnet-functions-dev \
-  --subnet subnet-private-endpoints \
-  --private-connection-resource-id $(az servicebus namespace show \
-      --name sb-dev-webber \
+  --name $FUNC_GATEWAY \
+  --vnet $VNET_NAME \
+  --subnet $SUBNET_FUNCTIONS
+
+az functionapp vnet-integration add \
+  --resource-group $RESOURCE_GROUP \
+  --name $FUNC_COURSE \
+  --vnet $VNET_NAME \
+  --subnet $SUBNET_FUNCTIONS
+
+az functionapp vnet-integration add \
+  --resource-group $RESOURCE_GROUP \
+  --name $FUNC_NOTIFICATION \
+  --vnet $VNET_NAME \
+  --subnet $SUBNET_FUNCTIONS
+  
+az functionapp vnet-integration add \
+  --resource-group $RESOURCE_GROUP \
+  --name $FUNC_REPORT \
+  --vnet $VNET_NAME \
+  --subnet $SUBNET_FUNCTIONS
+
+#================================================
+# Criar Private Endpoint das Functions privadas 
+#===============================================
+az network private-endpoint create \
+  --name pe-func-$FUNC_COURSE \
+  --resource-group $RESOURCE_GROUP \
+  --vnet-name $VNET_NAME \
+  --subnet $SUBNET_PRIVATE_ENDPOINT \
+  --private-connection-resource-id $(az functionapp show \
+      --name $FUNC_COURSE \
       --resource-group $RESOURCE_GROUP \
       --query id -o tsv) \
-  --group-id namespace \
-  --connection-name pe-sb-connection
+  --group-id sites \
+  --connection-name pe-func-$FUNC_COURSE-conn
+  
+az network private-endpoint create \
+  --name pe-func-$FUNC_NOTIFICATION \
+  --resource-group $RESOURCE_GROUP \
+  --vnet-name $VNET_NAME \
+  --subnet $SUBNET_PRIVATE_ENDPOINT \
+  --private-connection-resource-id $(az functionapp show \
+      --name $FUNC_NOTIFICATION \
+      --resource-group $RESOURCE_GROUP \
+      --query id -o tsv) \
+  --group-id sites \
+  --connection-name pe-func-$FUNC_NOTIFICATION-conn
+  
+az network private-endpoint create \
+  --name pe-func-$FUNC_REPORT \
+  --resource-group $RESOURCE_GROUP \
+  --vnet-name $VNET_NAME \
+  --subnet $SUBNET_PRIVATE_ENDPOINT \
+  --private-connection-resource-id $(az functionapp show \
+      --name $FUNC_REPORT \
+      --resource-group $RESOURCE_GROUP \
+      --query id -o tsv) \
+  --group-id sites \
+  --connection-name pe-func-$FUNC_REPORT-conn
 
-Associar ao DNS:
+# Associar DNS: OK
 az network private-endpoint dns-zone-group create \
   --resource-group $RESOURCE_GROUP \
-  --endpoint-name pe-servicebus \
-  --name sb-dns-group \
-  --private-dns-zone privatelink.servicebus.windows.net \
-  --zone-name privatelink.servicebus.windows.net
-
-
-4. Integrar Functions privadas à VNet [3] OK
-Para Função B, C e D: (repete para func-c e func-d)
-az functionapp vnet-integration add \
-  --resource-group $RESOURCE_GROUP \
-  --name func-api-gateway-webber \
-  --vnet vnet-functions-dev \
-  --subnet subnet-functions
-
-az functionapp vnet-integration add \
-  --resource-group $RESOURCE_GROUP \
-  --name func-course-rating-may \
-  --vnet vnet-functions-dev \
-  --subnet subnet-functions
-
-az functionapp vnet-integration add \
-  --resource-group $RESOURCE_GROUP \
-  --name func-notification-raysse \
-  --vnet vnet-functions-dev \
-  --subnet subnet-functions
-  
-az functionapp vnet-integration add \
-  --resource-group $RESOURCE_GROUP \
-  --name func-weekly-report-Math \
-  --vnet vnet-functions-dev \
-  --subnet subnet-functions
-
-
-5. Criar Private Endpoint das Functions privadas (repete para C e D)
-az network private-endpoint create \
-  --name pe-func-b \
-  --resource-group $RESOURCE_GROUP \
-  --vnet-name vnet-functions-dev \
-  --subnet subnet-private-endpoints \
-  --private-connection-resource-id $(az functionapp show \
-      --name func-course-rating-may \
-      --resource-group $RESOURCE_GROUP \
-      --query id -o tsv) \
-  --group-id sites \
-  --connection-name pe-func-b-conn
-  
-az network private-endpoint create \
-  --name pe-func-c \
-  --resource-group $RESOURCE_GROUP \
-  --vnet-name vnet-functions-dev \
-  --subnet subnet-private-endpoints \
-  --private-connection-resource-id $(az functionapp show \
-      --name func-notification-raysse \
-      --resource-group $RESOURCE_GROUP \
-      --query id -o tsv) \
-  --group-id sites \
-  --connection-name pe-func-c-conn
-  
-az network private-endpoint create \
-  --name pe-func-d \
-  --resource-group $RESOURCE_GROUP \
-  --vnet-name vnet-functions-dev \
-  --subnet subnet-private-endpoints \
-  --private-connection-resource-id $(az functionapp show \
-      --name func-weekly-report-Math \
-      --resource-group $RESOURCE_GROUP \
-      --query id -o tsv) \
-  --group-id sites \
-  --connection-name pe-func-d-conn
-
-Associar DNS: OK
-az network private-endpoint dns-zone-group create \
-  --resource-group $RESOURCE_GROUP \
-  --endpoint-name pe-func-b \
+  --endpoint-name pe-func-$FUNC_COURSE \
   --name func-b-dns \
   --private-dns-zone privatelink.azurewebsites.net \
   --zone-name privatelink.azurewebsites.net
   
 az network private-endpoint dns-zone-group create \
   --resource-group $RESOURCE_GROUP \
-  --endpoint-name pe-func-c \
+  --endpoint-name pe-func-$FUNC_NOTIFICATION \
   --name func-c-dns \
   --private-dns-zone privatelink.azurewebsites.net \
   --zone-name privatelink.azurewebsites.net
   
 az network private-endpoint dns-zone-group create \
   --resource-group $RESOURCE_GROUP \
-  --endpoint-name pe-func-d \
+  --endpoint-name pe-func-$FUNC_REPORT \
   --name func-d-dns \
   --private-dns-zone privatelink.azurewebsites.net \
   --zone-name privatelink.azurewebsites.net
 
-6. Bloquear acesso público das Functions privadas
+#=================================
+# Bloquear acesso público das Functions privadas
+#=================================
 az functionapp config access-restriction add \
   --resource-group $RESOURCE_GROUP \
-  --name func-course-rating-may \
+  --name $FUNC_COURSE \
   --rule-name "PermitirVNet" \
   --action Allow \
-  --vnet-name vnet-functions-dev \
-  --subnet subnet-functions \
+  --vnet-name $VNET_NAME \
+  --subnet $SUBNET_FUNCTIONS \
   --priority 100
   
 az functionapp config access-restriction add \
   --resource-group $RESOURCE_GROUP \
-  --name func-notification-raysse \
+  --name $FUNC_NOTIFICATION \
   --rule-name "PermitirVNet" \
   --action Allow \
-  --vnet-name vnet-functions-dev \
-  --subnet subnet-functions \
+  --vnet-name $VNET_NAME \
+  --subnet $SUBNET_FUNCTIONS \
   --priority 100
   
 az functionapp config access-restriction add \
   --resource-group $RESOURCE_GROUP \
-  --name func-weekly-report-Math \
+  --name $FUNC_REPORT \
   --rule-name "PermitirVNet" \
   --action Allow \
-  --vnet-name vnet-functions-dev \
-  --subnet subnet-functions \
+  --vnet-name $VNET_NAME \
+  --subnet $SUBNET_FUNCTIONS \
   --priority 100
